2d2beef1c032a7ae16aaceeeeb9eae24131f7490
andrearuocco <ruoca96@gmail.com>
2024-06-11 15:45:52 +0200

    week4M4

---
+++ script/script.js
@@ -0,0 +1,43 @@
+const URL = "https://striveschool-api.herokuapp.com/api/product"
+let token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjY4NDc2MDhmYzBmMzAwMTU1ZTVhY2UiLCJpYXQiOjE3MTgxMTAwNDgsImV4cCI6MTcxOTMxOTY0OH0.w82hZZEdNUe6s6wiIGb_yBzunMmdHcPLVETAZ85aYgI"
+
+const getData = async () => {
+    const data = await fetch(URL, {
+        headers: {
+            "Authorization": token
+        }
+    })
+    const response = await data.json()
+    return response
+}
+
+//getData().then(data => {
+//    console.log(data)
+//})
+
+var productO = {
+    name: "3310 cellphone",
+    description: "An unforgettable icon.",
+    brand: "Nokia",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Nokia_6110_blue-92107.jpg/800px-Nokia_6110_blue-92107.jpg",
+    price: 2000
+}
+
+const addProduct = async function(product) {
+    const added = await fetch(URL, {
+        method: "POST",
+        headers: {
+            "Authorization": token,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    /*addProduct(productO)*/
+    getData().then(data => {
+        console.log(data)
+    })
+})
+
